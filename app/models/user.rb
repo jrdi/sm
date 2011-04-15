f15ffc7cb4f5555d3d4cbc1f7c3f6bbd0cf4fe69
Jordi Villar <showlnx@gmail.com>
2011-04-15 20:49:54 +0200

    making user confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,23 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :trackable#, :validatable
+  devise :database_authenticatable, :registerable, :confirmable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  protected
   
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     data = access_token['extra']['user_hash']
     if user = User.find_by_email(data["email"])
       user
     else # Create a user with a stub password. 
-      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20]) 
+      user = User.new(:email => data["email"], :password => Devise.friendly_token[0,20])
+      user.skip_confirmation!
+      user.save 
+      user
     end
   end
   
@@ -21,7 +26,10 @@ class User < ActiveRecord::Base
     if user = User.find_by_email("#{data['screen_name']}@example.com")
       user
     else # Create a user with a stub password. 
-      User.create!(:email => "#{data['screen_name']}@example.com", :password => Devise.friendly_token[0,20]) 
+      user = User.new(:email => "#{data['screen_name']}@example.com", :password => Devise.friendly_token[0,20])
+      user.skip_confirmation!
+      user.save
+      user
     end
   end
   
@@ -37,5 +45,5 @@ class User < ActiveRecord::Base
   
   def password_required?
     new_record?
-   end
+  end
 end
