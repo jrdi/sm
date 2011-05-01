17367cf156c65faea843c40ed34c98452b19e701
Jordi Villar <showlnx@gmail.com>
2011-05-01 14:42:09 +0200

    last questions list

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,6 @@
 class SiteController < ApplicationController
   skip_before_filter :authenticate_user!, :only => [:home]
   def home
-    
+    @questions = Question.all(:order => 'created_at DESC')
   end
 end
