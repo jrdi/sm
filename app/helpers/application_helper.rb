7454d075d8e49fdbac9c9a0f584b7148b7681e7c
Jordi Villar <showlnx@gmail.com>
2011-05-04 00:43:48 +0200

    Add humanize_time_ago helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 module ApplicationHelper
   def title(title)
     content_for :title do
@@ -9,6 +11,16 @@ module ApplicationHelper
   def selected?(controller, action)
     if params[:controller] == controller && params[:action] == action
       return :class => 'selected'
+    end
+  end
+  
+  def humanize_time_ago(time)
+    if 0 != days = ((Time.now-time)/3600/24).floor
+      pluralize( days , 'día', 'días' )
+    elsif 0 != hours = ((Time.now-time)/3600).floor
+      pluralize( hours , 'hora', 'horas' )
+    else
+      pluralize( (Time.now-time).floor, 'segundo', 'segundos' )
     end
   end
 end
