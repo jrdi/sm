c89db5944a81834e4d1dbb1d92a7fa212cdfb5a5
Jordi Villar <showlnx@gmail.com>
2011-05-04 17:39:45 +0200

    configure :es locales and using time_ago_in_words

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,16 +13,4 @@ module ApplicationHelper
       return :class => 'selected'
     end
   end
-  
-  def humanize_time_ago(time)
-    if 0 != days = ((Time.now-time)/3600/24).floor
-      pluralize( days , 'día', 'días' )
-    elsif 0 != hours = ((Time.now-time)/3600).floor
-      pluralize( hours , 'hora', 'horas' )
-    elsif 0 != minutes = ((Time.now-time)/60).floor
-      pluralize( minutes, 'minuto', 'minutos' )
-    else
-      "menos de un minuto."
-    end
-  end
 end
