32c7283d02e98506edbabb9c19544ef52fee7618
Jordi Villar <showlnx@gmail.com>
2011-05-06 17:17:29 +0200

    home: links to change of timeline

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -11,13 +11,13 @@ class SiteController < ApplicationController
     
     case params[:scoped_questions]
     when 'without_answers'
-      @questions      = Question.without_answers(:limit => "#{page}, 10")
+      @questions        = Question.without_answers(:limit => "#{page}, 10")
       @question_pages ||= (Question.without_answers_count/10.0).floor+1
     when 'populars'
-      @questions      = Question.populars(:limit => "#{page}, 10")
+      @questions        = Question.populars(:limit => "#{page}, 10")
       @question_pages ||= (Question.populars_count/10.0).floor+1
     else
-      @questions      = Question.all(:order => 'created_at DESC', :include => :user, :limit => "#{page}, 10")
+      @questions        = Question.all(:order => 'created_at DESC', :include => :user, :limit => "#{page}, 10")
       @question_pages ||= (Question.count/10.0).floor+1
     end
     
