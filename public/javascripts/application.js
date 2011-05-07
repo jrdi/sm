cdc669e1fe9ae23895a51e27695ff818d3ea098b
matallo <matallo@gmail.com>
2011-05-07 03:09:14 +0200

    mejora llamada ajax pasando parametros de la url a la funcion y sin hacer llamadas extra

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   page = {
     currentPage: 1,
-    lastPage: ""
+    lastPage: "",
+    scope: ""
   }
-  scope=""
   moreItems = [];
 
   $.urlParam = function(name){
@@ -21,7 +21,7 @@ $(document).ready(function() {
   }
 
   if($.urlParam('scoped_questions') != 0) {
-    scope = $.urlParam('scoped_questions');
+    page.scope = $.urlParam('scoped_questions');
   }
 
   $.getQuestions = function() {
@@ -31,16 +31,13 @@ $(document).ready(function() {
       data: {
         page: page.currentPage+1,
         questions_pages: page.lastPage,
-        scoped_questions: scope
+        scoped_questions: page.scope
       },
       success: function(data) {
-        if(page.currentPage === data[1]) {
-          $('.load-more').addClass("disabled");
-        } else {
-          $.each(data[0], function(key, value) {
-            moreItems.push('<li class="question"><a title="Fav" class="command-item star" href="/"><span>☆</span></a><div class="commands"><div class="replies clearfix"><a title="Ve a las respuestas" class="comments" href="/questions/' + value["question"].id + '">' + value["question"].answers_count + '</a><a title="Responder" class="command-item reply" href="/"><span>Responder</span></a></div></div><h2 class="question-title"><a title="Enlace permanente a ' + value["question"].title + '" href="/questions/' + value["question"].id + '">' + value["question"].title + '</a></h2><p class="question-description">' + value["question"].description + '</p><div class="question-meta"><ul class="tags"><li class="tag"><a title="Sigue el tema Cocina" class="tag-follow" href="/"><span>+</span></a><a title="Ve al tema Cocina" class="tag-link" href="/">Cocina</a></li><li class="tag"><a title="Sigue el tema Limpieza" class="tag-follow tag-followed" href="/"><span>+</span></a><a title="Ve al tema Limpieza" class="tag-link" href="/">Limpieza</a></li><li class="tag"><a title="Sigue el tema Dinero" class="tag-follow" href="/"><span>+</span></a><a title="Ve al tema Dinero" class="tag-link" href="/">Dinero</a></li></ul><p>Por <a href="/">' + value["question"]["user"].name + '</a> ' + value["question"].created_at + '</p></div></li>');
-          });
-        }
+        page.lastPage = data[1];
+        $.each(data[0], function(key, value) {
+          moreItems.push('<li class="question"><a title="Favorito" class="command-item star" href="/"><span>☆</span></a><div class="commands"><div class="replies clearfix"><a title="Ir a las respuestas" class="comments" href="/questions/' + value["question"].id + '">' + value["question"].answers_count + '</a><a title="Responder" class="command-item reply" href="/"><span>Responder</span></a></div></div><h2 class="question-title"><a title="Enlace permanente a ' + value["question"].title + '" href="/questions/' + value["question"].id + '">' + value["question"].title + '</a></h2><p class="question-description">' + value["question"].description + '</p><div class="question-meta"><ul class="tags"><li class="tag"><a title="Seguir el tema Cocina" class="tag-follow" href="/"><span>+</span></a><a title="Ir al tema Cocina" class="tag-link" href="/">Cocina</a></li><li class="tag"><a title="Seguir el tema Limpieza" class="tag-follow tag-followed" href="/"><span>+</span></a><a title="Ir al tema Limpieza" class="tag-link" href="/">Limpieza</a></li><li class="tag"><a title="Seguir el tema Dinero" class="tag-follow" href="/"><span>+</span></a><a title="Ir al tema Dinero" class="tag-link" href="/">Dinero</a></li></ul><p>Por <a href="/">' + value["question"]["user"].name + '</a> ' + value["question"].created_at + '</p></div></li>');
+        });
       }
     });
   };
@@ -48,8 +45,15 @@ $(document).ready(function() {
   $.getQuestions();
 
   $('.load-more').live("click", function() {
-    page.currentPage++;
-    $('#questions-list').append(moreItems.join(''));
-    $.getQuestions();
+    if(page.currentPage+1 <= page.lastPage) {
+      $('#questions-list').append(moreItems.join(''));
+      moreItems = [];
+      page.currentPage++;
+      if(page.currentPage+1 === page.lastPage) {
+        $.getQuestions();
+      }
+    } else {
+      alert("Has llegado al final, campeon!")
+    }
   });
 });
