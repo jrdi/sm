5f41f0a1118b8a92dfab239074b5ec63e2c42cd4
matallo <matallo@gmail.com>
2011-05-07 05:21:09 +0200

    prevent duplicate when changing scope and loading more

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -51,6 +51,7 @@ $(document).ready(function() {
           Questions.push('<li class="question"><a title="Favorito" class="command-item star" href="/"><span>☆</span></a><div class="commands"><div class="replies clearfix"><a title="Ir a las respuestas" class="comments" href="/questions/' + value["question"].id + '">' + value["question"].answers_count + '</a><a title="Responder" class="command-item reply" href="/"><span>Responder</span></a></div></div><h2 class="question-title"><a title="Enlace permanente a ' + value["question"].title + '" href="/questions/' + value["question"].id + '">' + value["question"].title + '</a></h2><p class="question-description">' + value["question"].description + '</p><div class="question-meta"><ul class="tags"><li class="tag"><a title="Seguir el tema Cocina" class="tag-follow" href="/"><span>+</span></a><a title="Ir al tema Cocina" class="tag-link" href="/">Cocina</a></li><li class="tag"><a title="Seguir el tema Limpieza" class="tag-follow tag-followed" href="/"><span>+</span></a><a title="Ir al tema Limpieza" class="tag-link" href="/">Limpieza</a></li><li class="tag"><a title="Seguir el tema Dinero" class="tag-follow" href="/"><span>+</span></a><a title="Ir al tema Dinero" class="tag-link" href="/">Dinero</a></li></ul><p>Por <a href="/">' + value["question"]["user"].name + '</a> ' + value["question"].created_at + '</p></div></li>');
         });
         $('#questions-list').html(Questions.join(''));
+        $.resetQuestions();
       }
     });
   };
@@ -105,5 +106,6 @@ $(document).ready(function() {
     $.resetQuestions();
     page.scope = $(this).attr('id');
     $.loadQuestions();
+    $.loadMoreQuestions();
   });
 });
