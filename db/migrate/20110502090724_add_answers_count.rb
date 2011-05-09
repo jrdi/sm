fbd62cc2b866243c81e64035f492454cea2a1aa6
Jordi Villar <showlnx@gmail.com>
2011-05-09 13:03:25 +0200

    remove some migration

--- db/migrate/20110502090724_add_answers_count.rb
+++ db/migrate/20110502090724_add_answers_count.rb
@@ -1,11 +1,11 @@
 class AddAnswersCount < ActiveRecord::Migration
   def self.up
     add_column :questions, :answers_count, :integer, :default => 0
-    
-    Question.reset_column_information
-    Question.all.each do |q|
-      Question.update_counters q.id, :answers_count => q.answers.length
-    end  
+    # 
+    # Question.reset_column_information
+    # Question.all.each do |q|
+    #   Question.update_counters q.id, :answers_count => q.answers.length
+    # end  
   end
 
   def self.down
