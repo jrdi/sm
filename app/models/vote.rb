eeeb28e2ca68c5756e199a0e713b92ca0bd042d0
Jordi Villar <showlnx@gmail.com>
2011-06-01 10:51:54 +0200

    votes_count control

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,7 @@
 class Vote < ActiveRecord::Base
   # Relations
   belongs_to :user
-  belongs_to :votable, :polymorphic => true, :counter_cache => true
+  belongs_to :votable, :polymorphic => true
   # Validations
   validates_presence_of :votable_id, :votable_type, :user_id, :value
   validates_uniqueness_of :user_id, :scope => [:votable_id, :votable_type]
