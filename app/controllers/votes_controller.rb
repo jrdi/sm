a41234f24e2c2404af7d195c0d37f6d621672667
Jordi Villar <showlnx@gmail.com>
2011-06-02 19:09:33 +0200

    votes with js.erb

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,12 +6,12 @@ class VotesController < ApplicationController
     if @vote.save
       respond_to do |format|
         format.html { redirect_to(root_url, :notice => 'El voto ha sido guardado correctamente.') }
-        format.json { render :json => @vote, :status => :created }
+        format.js
       end
     else
       respond_to do |format|
         format.html { redirect_to(root_url, :alert => 'El voto no ha podido ser guardado correctamente.') }
-        format.json { render :json => @vote.errors, :status => :unprocessable_entity }
+        format.js
       end
     end
   end
--- public/javascripts/question.js
+++ public/javascripts/question.js
@@ -1,37 +1,4 @@
-// init variables
-// Answers = [];   
-
 $(document).ready(function() {
-  // // init functions
-  // $.loadAnswers = function() {
-  //   $.ajax({
-  //     type: "GET",
-  //     dataType: 'json',
-  //     success: function(data) {
-  //       $.each(data["question"]["answers"], function(aKey, aVal) {
-  //         Answers.push(aVal);
-  //       });
-  //     }
-  //   });
-  // };
-  // 
-  // votes_count = function(a,b){
-  //   if(a.votes_count < b.votes_count) {
-  //     return 1;
-  //   } else if(a.votes_count == b.votes_count) {
-  //     return a.created_at > b.created_at ? 1 : -1;  
-  //   } else {
-  //     return -1;
-  //   }
-  // };  
-  // 
-  // created_at = function(a,b){
-  //   return a.created_at < b.created_at ? 1 : -1;  
-  // };
-  // 
-  // // fire functions
-  // $.loadAnswers();
-
   $('.answers-header-menu-item').live("click", function(e) {
     e.preventDefault();
     clickedItem = $(this);
@@ -47,46 +14,4 @@ $(document).ready(function() {
     $("#answer_content").focus();
     return false;
   });
-  
-  $('.command-item').live("click", function(e) {
-    e.preventDefault();
-  
-    answerId = $(this).parent().parent().parent().attr('id');
-    vote = [];
-    vote["vote[votable_id]"] = answerId.replace(/answer-/, '');
-  
-    if($(this).hasClass('up')) {
-      vote["vote[value]"] = 1;
-    } else if($(this).hasClass('down')) {
-      vote["vote[value]"] = -1;
-    }
-  
-    $.each($('#' + answerId + ' form #vote_value[value="' + vote["vote[value]"] + '"]').parent().serializeArray(), function(pKey,pValue) { vote[pValue.name] = pValue.value; });
-  
-    $.ajax({
-      type: "POST",
-      url: "/votes.json",
-      data: {
-        "vote": {
-          "user_id": vote["vote[user_id]"],
-          "value": vote["vote[value]"],
-          "votable_type": vote["vote[votable_type]"],
-          "votable_id": vote["vote[votable_id]"]
-        }
-      },
-      success: function(data) {
-        if(vote["vote[value]"] === "1") {
-          $('#' + answerId + ' .up').addClass('voted');
-        } else if(vote["vote[value]"] === "-1") {
-          $('#' + answerId + ' .down').addClass('voted');
-        }
-        alert('Tu voto ha sido recibido, gracias!');
-      },
-      statusCode: {
-        422: function() {
-          alert('No hemos podido recibir tu voto, seguramente ya has votado antes, lo sentimos!');
-        }
-      }
-    });      
-  });                  
 });
