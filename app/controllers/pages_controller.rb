11acd0d5be2f37a0fa2c9c8a78dafb0da0ab54be
Jordi Villar <jrdi.villar@gmail.com>
2012-11-01 16:42:59 +0100

    home: improve questions index performance

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,18 +6,19 @@ class PagesController < ApplicationController
       @question = Question.new
     end
     
-    page            = params[:page].present? ? (params[:page].to_i-1)*10 : 0
+    offset          = params[:page].present? ? (params[:page].to_i-1)*10 : 0
     @question_pages = params[:question_pages].present? ? params[:question_pages].to_i : nil
-
+    @questions      = Question.includes(:user, :answers, :tags).limit(10).offset(offset)
+    
     case params[:scoped_questions]
     when 'without_answers'
-      @questions        = Question.without_answers(:limit => "10", :offset => page)
-      @question_pages ||= (Question.without_answers_count/10.0).ceil
+      @questions        = @questions.without_answers
+      @question_pages ||= (Question.without_answers.count/10.0).ceil
     when 'populars'
-      @questions        = Question.populars(:limit => "10", :offset => page)
-      @question_pages ||= (Question.populars_count/10.0).ceil
+      @questions        = @questions.populars
+      @question_pages ||= (Question.populars.count/10.0).ceil
     else
-      @questions        = Question.all(:order => 'created_at DESC', :include => [:user, :answers, :tags], :limit => "10", :offset => page)
+      @questions        = @questions.order('created_at DESC')
       @question_pages ||= (Question.count/10.0).ceil
     end
   end
