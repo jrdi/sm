dd9fac6d4121c5c6552ee47681ac519407743829
Carlos Matallín <matallo@gmail.com>
2012-11-11 18:05:47 +0100

    habemus dropdowns

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,8 @@
-//= require js-url.min
 //= require modernizr
 //= require jquery
 //= require jquery_ujs
+//= require js-url.min
+//= require jquery.qtip.min
 //= require_tree .
 
 $(document).ready(function() {
@@ -13,9 +14,37 @@ $(document).ready(function() {
     $(".notice").show().delay(5000).fadeOut();
   }
 
+  $('.navbar-js').on('click', '.dropdown_link-js', function(e) {
+    e.preventDefault();
+  });
+
+  $('.dropdown_link-js').qtip({
+    overwrite: false,
+    content: {
+      text: $('.navbar_dropdown')
+    },
+    position: {
+      my: 'top right',
+      at: 'bottom right',
+      adjust: { x: -8, y: +5 }
+    },
+    show: {
+      event: 'click',
+      effect: function() { $(this).show().css('opacity', '0').animate({ opacity: 1, "top": "-=10px" }, { duration: 200 }); }
+    },
+    hide: {
+      event: 'unfocus click',
+      effect: function() { $(this).animate({ opacity: 0, "top": "+=10px" }, { duration: 200 }); }
+    },
+    style: {
+      classes: 'ui-tooltip-dropdown',
+      tip: { width: 18, height: 10, corner: 'top right',  mimic: 'center', offset: 5 }
+    }
+  });
+
   $('.questions_header-js, .answers_header-js').on("click", ".tab-js", function(e){
     clickedItem = $(this);
-    selectedItem = $(this).parent().parent().find(".selected");
+    selectedItem = $(this).closest('.menu').find(".selected");
 
     if (!$(this).is(".selected")){
       $(selectedItem).removeClass('selected');
